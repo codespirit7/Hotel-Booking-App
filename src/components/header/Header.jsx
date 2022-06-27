9e68877b9aca926f70395df076426b24e9d58a01
codespirit7 <singhriteshs1429@gmail.com>
2022-06-28 01:12:58 +0530

    NavBar added

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='headerList'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../../components/header/Header'
+import Navbar from '../../components/navbar/Navbar'
+
+import './home.css'
 
 export function Home() {
   return (
-    <div>Home</div>
+    <div><Navbar/>
+    <Header/></div>
   )
 }
 
